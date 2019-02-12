e6775fe3217d7feba5ce04666b21424f6207f0fb
Muhammad Amal [MY] <MAmal@kerrylogistics.com>
2019-02-12 17:18:26 +0800

    Add project files.

---
+++ my_first_web_app/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(my_first_web_app.Startup))]
+namespace my_first_web_app
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
